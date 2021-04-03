c8ccb0a0f80e4d4c46c0d01de5d294e229ce5b13
Shree <Shree@DESKTOP-C63B4ID>
2021-04-03 14:01:54 +0530

    Develop by me

--- GitDemoProject/src/GitDemo/GitHome.java
+++ GitDemoProject/src/GitDemo/GitHome.java
@@ -2,6 +2,11 @@ package GitDemo;
 
 public class GitHome {
 	
+	public void sum()
+	{
+		System.out.println("sum..GitHome");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Simple Git Demo project");
